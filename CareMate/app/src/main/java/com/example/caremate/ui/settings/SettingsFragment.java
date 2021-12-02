11be296bb2a1aa4caba841df28485bd46a3eaf1f
labbloe <54924769+labbloe@users.noreply.github.com>
2021-12-02 00:20:02 -0600

    Added WiFi configurable settings in settings menu

--- CareMate/app/src/main/java/com/example/caremate/ui/settings/SettingsFragment.java
+++ CareMate/app/src/main/java/com/example/caremate/ui/settings/SettingsFragment.java
@@ -1,9 +1,11 @@
 package com.example.caremate.ui.settings;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.caremate.MainActivity;
 import com.example.caremate.R;
 import com.example.caremate.databinding.FragmentSettingsBinding;
 
@@ -19,6 +22,9 @@ public class SettingsFragment extends Fragment {
 
     private SettingsViewModel settingsViewModel;
     private FragmentSettingsBinding binding;
+    private MainActivity main;
+    public MainActivity.ConnectThread conn;
+    private Button saveButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,15 +33,30 @@ public class SettingsFragment extends Fragment {
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        main = (MainActivity) getActivity();
+        return root;
+    }
 
-        final TextView textView = binding.textGallery;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView ssid = (TextView) getView().findViewById(R.id.wifi_ssid);
+        TextView pass = (TextView) getView().findViewById(R.id.wifi_password);
+
+        conn = MainActivity.conn;
+        saveButton = (Button) getView().findViewById(R.id.settingsSave);
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                String wifiSSID = ssid.getText().toString();
+                String wifiPASS = pass.getText().toString();
+                String msg = "{type:wifi,SSID:" + wifiSSID + ",pass:" + wifiPASS + "}";
+                conn.sendData(msg);
+                Log.w("click", msg);
+                //conn.sendData("{bin1,monday-1159,bin2,tuesday-0800,wednesday-0830}");
             }
         });
-        return root;
     }
 
     @Override
